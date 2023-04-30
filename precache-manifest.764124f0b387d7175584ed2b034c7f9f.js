b0bfca4aedf10817b047d3b0de5a92fbd170e233
Griffith Prendiville <griffithprendiville@gmail.com>
2023-04-30 10:01:26 -0700

    Updates

---
+++ precache-manifest.764124f0b387d7175584ed2b034c7f9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d9708e37fac5ed1b5f0",
+    "url": "/gprendiville/static/css/main.fc753516.chunk.css"
+  },
+  {
+    "revision": "4d9708e37fac5ed1b5f0",
+    "url": "/gprendiville/static/js/main.7a1f5601.chunk.js"
+  },
+  {
+    "revision": "66513ca7619c3d1e06b6",
+    "url": "/gprendiville/static/js/runtime~main.f097f522.js"
+  },
+  {
+    "revision": "0335c0a13fecfb151ee5",
+    "url": "/gprendiville/static/js/2.49cb00a6.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/gprendiville/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/gprendiville/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "0a007c9b79996932f954c7ae84648e74",
+    "url": "/gprendiville/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.79fe092024bf1d79bdf785bdad064f49.js"
+  "/gprendiville/precache-manifest.764124f0b387d7175584ed2b034c7f9f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/gprendiville/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });

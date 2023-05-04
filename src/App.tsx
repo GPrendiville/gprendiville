b5f476f5d02d65281023a8737179db134b4080d5
Griffith Prendiville <griffithprendiville@gmail.com>
2023-05-03 22:46:35 -0700

    Added name and link to github

--- src/App.tsx
+++ src/App.tsx
@@ -8,17 +8,16 @@ function App() {
   return (
     <div >
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+        <p className="Name">
+          Griffith Prendiville
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/GPrendiville"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Github
         </a>
       </header>
     </div>
